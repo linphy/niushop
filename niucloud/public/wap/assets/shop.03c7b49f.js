f9b4f125e7ca32a9a437dee246fbd8f64aa9ec3f
全栈小学生 <1518079521@qq.com>
2024-03-09 15:50:49 +0800

    update wap

--- niucloud/public/wap/assets/shop.03c7b49f.js
+++
@@ -1,1 +0,0 @@
-import{b1 as o}from"./index-616d13f7.js";function e(){return o.get("shop/goods/evaluate/config")}export{e as g};
---
+++ niucloud/public/wap/assets/shop.15cdf37d.js
@@ -0,0 +1,1 @@
+import{b0 as o}from"./index-b892d4e0.js";function e(){return o.get("shop/goods/evaluate/config")}export{e as g};
