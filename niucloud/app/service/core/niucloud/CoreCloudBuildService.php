3af9a6ff33070978c4966c58c1193270f1d2c618
全栈小学生 <1518079521@qq.com>
2024-05-17 11:42:31 +0800

    Update CoreCloudBuildService.php

--- niucloud/app/service/core/niucloud/CoreCloudBuildService.php
+++ niucloud/app/service/core/niucloud/CoreCloudBuildService.php
@@ -137,7 +137,7 @@ class CoreCloudBuildService extends BaseCoreService
                     'filename' => 'build.zip'
                 ]
             ],
-            'timeout' => 50.0
+            'timeout' => 0
         ]);
         if (isset($response['code']) && $response['code'] == 0) throw new CommonException($response['msg']);
 
@@ -201,7 +201,7 @@ class CoreCloudBuildService extends BaseCoreService
                     'filename' => 'build.zip'
                 ]
             ],
-            'timeout' => 50.0
+            'timeout' => 0
         ]);
         if (isset($response['code']) && $response['code'] == 0) throw new CommonException($response['msg']);
 
